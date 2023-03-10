6b67648e42fb35fbc964050b928e0b9a20b5f1f2
IDGS-801-18002424 <18002424@alumnos.utleon.edu.mx>
2023-03-10 09:14:59 -0600

    09/03/23

--- app.py
+++ app.py
@@ -8,58 +8,90 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect()
 
-#----------------------------------------RUTAS-------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------RUTAS-------------------------------------------------------------------------------------------------------------------
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     create_form = forms.UserForm(request.form)
-    
+
     if request.method == 'POST':
-        alumn = Alumnos(nombre = create_form.nombre.data,
-                        apellidos = create_form.apellidos.data,
-                        email = create_form.email.data)
+        alumn = Alumnos(nombre=create_form.nombre.data,
+                        apellidos=create_form.apellidos.data,
+                        email=create_form.email.data)
         db.session.add(alumn)
         db.session.commit()
-    
-    return render_template('index.html', name = 'Inicio', form = create_form)
+
+    return render_template('index.html', name='Inicio', form=create_form)
+
+# ---------------------------------------------------- GET ALL ---------------------------------------------------------------------------------------
+
 
 @app.route('/ABCompleto', methods=['GET', 'POST'])
 def abcompleto():
     create_form = forms.UserForm(request.form)
-    #Select * from alumnos
+    # Select * from alumnos
     alumnos = Alumnos.query.all()
     return render_template('ABCompleto.html', form=create_form, alumnos=alumnos, name="ABCompleto")
 
-@app.route('/modificar')
-def modificar():
-    create_form = forms.UserForm(request.form)
-    id = request.args.get('id')
-    #SELECT * FROM alumnos where id==id
-    alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-    create_form.nombre.data = alumno.nombre
-    create_form.apellidos.data = alumno.apellidos
-    create_form.email.data = alumno.email
-    
+# ---------------------------------------------------- MODIFICAR --------------------------------------------------------------------------------------
+
+
 @app.route('/modificar', methods=['POST'])
 def modificar():
     create_form = forms.UserForm(request.form)
-    id = request.args.get('id')
-    #SELECT * FROM alumnos where id==id
-    alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
-    alumno.nombre = create_form.nombre.data
-    alumno.apellidos = create_form.apellidos.data
-    alumno.email = create_form.email.data
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # SELECT * FROM alumnos where id==id
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = id
+        create_form.nombre.data = alumno.nombre
+        create_form.apellidos.data = alumno.apellidos
+        create_form.email.data = alumno.email
 
-    db.session.add(alumno)
-    db.session.commit()
-    
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alumno.nombre = create_form.nombre.data
+        alumno.apellidos = create_form.apellidos.data
+        alumno.email = create_form.email.data
+        db.session.add(alumno)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('modificar.html', form=create_form)
 
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------- ELIMINAR --------------------------------------------------------------------------------------
+
+
+@app.route('/eliminar', methods=['POST'])
+def eliminar():
+    create_form = forms.UserForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # SELECT * FROM alumnos where id==id
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = id
+        create_form.nombre.data = alumno.nombre
+        create_form.apellidos.data = alumno.apellidos
+        create_form.email.data = alumno.email
+
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alumno.nombre = create_form.nombre.data
+        alumno.apellidos = create_form.apellidos.data
+        alumno.email = create_form.email.data
+        db.session.delete(alumno)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('eliminar.html', form=create_form)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(host= "10.1.1.11", port=3000)
-    
+    app.run(port=3000)
---
+++ main.py
@@ -0,0 +1,16 @@
+from db import get_connection
+
+try:
+    connection = get_connection()
+    with connection.cursor() as curso:
+        curso.execute('call Consulta_maestros();')
+        resultset = curso.fetchall()
+        for row in resultset:
+            print(row)
+    connection.close()
+    
+except Exception as ex:
+    print('¡ERROR BRO! YA DATE DE BAJA MI PRRO')
+
+
+    #   Id | Nombre | Apellidos | Correo
